9e1bb9ac79bf53c0f1c6b4a00369ac873bdf74b2
jbelter <kuba.belter@gmail.com>
2020-07-27 00:45:55 +0200

    Add proper javadoc to the controller code

--- src/main/java/com/example/Meritus/WebApp/CalculatorController.java
+++ src/main/java/com/example/Meritus/WebApp/CalculatorController.java
@@ -8,16 +8,32 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * Simple Controller for the Calculator Application
+ */
 @Controller
 public class CalculatorController {
 
 
+    /**
+     * Displays calculation page
+     * @param model - provided by Spring, model of data
+     * @return - jsp page 'calculate.jsp'
+     */
     @GetMapping("/")
     public String showCalculator(Model model){
         model.addAttribute("calculator", new Calculator());
         return "calculate";
     }
 
+    /**
+     * Displays calculation result page if validation has passed
+     * @param calculator - provided in the showCalculator method to Spring
+     * @param bindingResult - provided in Spring
+     * @return - depending on errors
+     *          no errors - result.jsp
+     *          errors - calculate.jsp
+     */
     @PostMapping("/")
     public String doCalculation(@Valid @ModelAttribute("calculator") Calculator calculator,
                                 BindingResult bindingResult){
