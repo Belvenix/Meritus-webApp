2721b2bb45ac330984c9dc3fcd5fed4fd4c77588
jbelter <kuba.belter@gmail.com>
2020-07-27 00:45:11 +0200

    Remove redundant import and provide proper code documentation

--- src/main/java/com/example/Meritus/WebApp/Calculator.java
+++ src/main/java/com/example/Meritus/WebApp/Calculator.java
@@ -1,20 +1,28 @@
 package com.example.Meritus.WebApp;
 
 import org.hibernate.validator.constraints.Range;
-import org.springframework.format.annotation.NumberFormat;
 
 import javax.validation.constraints.*;
 
+/**
+ * JB which provides a service to the client
+ */
 public class Calculator {
 
+    //Parameter responsible for first input from the form; with validation
     @NotNull
     @Range(min = -1073741823, max = 1073741823)
     private Long first;
 
+    //Parameter responsible for second input from the form; with validation
     @NotNull
     @Range(min = -1073741823, max = 1073741823)
     private Long second;
 
+    /**
+     * returns the sum of numbers provided by user
+     * @return - sum of numbers
+     */
     public Long add(){
         if(first!=null && second != null)
             return first+second;
@@ -22,18 +30,23 @@ public class Calculator {
             return null;
     }
 
+
+    //Getter
     public Long getFirst() {
         return first;
     }
 
+    //Setter
     public void setFirst(Long first) {
         this.first = first;
     }
 
+    //Getter
     public Long getSecond() {
         return second;
     }
 
+    //Setter
     public void setSecond(Long second) {
         this.second = second;
     }
